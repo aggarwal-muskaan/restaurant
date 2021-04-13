2489587b2e9b6402e8786bdec89e26908927812b
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-13 19:04:49 +0530

    controlled input field in Navigation.js
    if key pressed is Enter, then show alert dialog box

--- src/components/restaurant/Navigation.js
+++ src/components/restaurant/Navigation.js
@@ -1,12 +1,25 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { toggleContext } from "../../contexts/drawer.context";
+
 function Navigation() {
   const showDrawer = useContext(toggleContext);
+  const [input, setInput] = useState("");
+  const handleChange = (event) => {
+    setInput(event.target.value);
+  };
+
   return (
     <div>
       <div>
         location
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Search for Restaurants (Press Enter to search)"
+          onChange={handleChange}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") alert(`you pressed Enter after ${input}`);
+          }}
+        />
         <button onClick={() => showDrawer(true)}>filter</button>
         <button>cart</button>
       </div>
