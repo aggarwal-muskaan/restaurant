3a5ebe45c80a787da9379ffc082d89c1a91b845e
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-13 14:15:19 +0530

    add loading spinner while fetching results from the API

--- src/components/restr-details/RestaurantDetails.js
+++ src/components/restr-details/RestaurantDetails.js
@@ -5,51 +5,61 @@ function RestaurantDetails() {
   let params = useParams();
   let history = useHistory();
   const [selectedRestr, setRestr] = useState([]);
+  const [spinner, setSpinner] = useState(true);
 
   const restrDetailsUrl =
     "https://api.sheety.co/bdcbafbc1f4197dda178b9e69f6ccee9/techAlchemyWebTest1/restaurantDetails";
 
   useEffect(() => {
     const getRestaurant = `${restrDetailsUrl}?filter[id]=${params.restrId}`;
+    // used template string for filtering restaurant on ID value
     fetch(getRestaurant)
       .then((res) => res.json())
-      // stores only first record from API
-      .then((data) => setRestr(data.restaurantDetails[0]))
+      .then((data) => {
+        // stores only first record from API
+        const askedRestr = data.restaurantDetails[0];
+        // updates state only if the Restaurant found
+        askedRestr && setRestr(askedRestr);
+        setSpinner(false);
+      })
       .catch((err) => console.log(err));
   }, [params]);
 
-  return selectedRestr.length !== 0 ? (
-    <div>
-      <div>
-        <button onClick={() => history.goBack()}>back</button>
-        <button>cart</button>
-      </div>
-
+  const printData =
+    selectedRestr.length !== 0 ? (
       <div>
         <div>
-          <h3>{selectedRestr.restaurantName}</h3>
-          <section>{selectedRestr.restaurantDescription}</section>
-          <div>
-            icon & <span>{selectedRestr.openingHours}</span>
-          </div>
-          <div>
-            icon & <span>{selectedRestr.contactNumber}</span>
-          </div>
-          <div>
-            icon & <span>{selectedRestr.websiteUrl}</span>
-          </div>
+          <button onClick={() => history.goBack()}>back</button>
+          <button>cart</button>
         </div>
-        <img
-          width="250px"
-          height="250px"
-          src={selectedRestr.restaurantImage}
-          alt={selectedRestr.restaurantName}
-        />
+
+        <div>
+          <div>
+            <h3>{selectedRestr.restaurantName}</h3>
+            <section>{selectedRestr.restaurantDescription}</section>
+            <div>
+              icon & <span>{selectedRestr.openingHours}</span>
+            </div>
+            <div>
+              icon & <span>{selectedRestr.contactNumber}</span>
+            </div>
+            <div>
+              icon & <span>{selectedRestr.websiteUrl}</span>
+            </div>
+          </div>
+          <img
+            width="250px"
+            height="250px"
+            src={selectedRestr.restaurantImage}
+            alt={selectedRestr.restaurantName}
+          />
+        </div>
       </div>
-    </div>
-  ) : (
-    <div>No Such restaurant</div>
-  );
+    ) : (
+      <div>No Such restaurant</div>
+    );
+
+  return spinner ? "Loading..." : printData;
 }
 
 export default RestaurantDetails;
