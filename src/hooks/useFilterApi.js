aac6b3629928b01f3fd2a237bff49f37ca0eebaa
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-15 12:41:35 +0530

    refactor variable name

--- src/hooks/useFilterApi.js
+++ src/hooks/useFilterApi.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { allRestaurantsUrl } from "../variables/baseUrl";
+import { ALL_RESTR_URL } from "../variables/baseUrl";
 
 function useFilterApi(init) {
   const [filterRestr, setState] = useState(init);
   const [newR, setNew] = useState([]);
   useEffect(() => {
-    const newUrl = `${allRestaurantsUrl}?filter[${filterRestr.filterBy}]=${filterRestr.value}`;
-    console.log("huree", newUrl);
+    const newUrl = `${ALL_RESTR_URL}?filter[${filterRestr.filterBy}]=${filterRestr.value}`;
+    // console.log("huree", newUrl);
     axios.get(newUrl).then((res) => {
       setNew(res.data.allRestaurants);
-      console.log(newR);
+      // console.log(newR);
     });
   }, [filterRestr]);
   return [newR, setState];
