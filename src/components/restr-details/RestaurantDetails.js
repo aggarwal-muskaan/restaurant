eec022be10c6a38178068ba043be4a2da4471a55
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-15 16:35:03 +0530

    imported API for menu feom variables/baseUrl.js and fetched menu of found restaurants

--- src/components/restr-details/RestaurantDetails.js
+++ src/components/restr-details/RestaurantDetails.js
@@ -1,30 +1,50 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { useParams, useHistory } from "react-router-dom";
 import { useSpinner } from "../../hooks/useSpinner";
+import { RESTR_DETAILS_URL, RESTR_MENU_DETAILS } from "../../variables/baseUrl";
 
 function RestaurantDetails() {
   let params = useParams();
   let history = useHistory();
   const [selectedRestr, setRestr] = useState([]);
+  const [isMenuOfRestr, foundMenu] = useState(false);
   const [spinner, handleSpinner] = useSpinner(true);
 
-  const restrDetailsUrl =
-    "https://api.sheety.co/bdcbafbc1f4197dda178b9e69f6ccee9/techAlchemyWebTest1/restaurantDetails";
+  const restrDetailsUrl = RESTR_DETAILS_URL;
 
-  useEffect(() => {
-    const getRestaurant = `${restrDetailsUrl}?filter[id]=${params.restrId}`;
-    // used template string for filtering restaurant on ID value
-    fetch(getRestaurant)
-      .then((res) => res.json())
-      .then((data) => {
+  useEffect(
+    () => {
+      const getRestaurant = `${restrDetailsUrl}?filter[id]=${params.restrId}`;
+      //* used template string for filtering restaurant on ID value
+
+      const getMenu = `${RESTR_MENU_DETAILS}?filter[restaurantName]=${selectedRestr.restaurantName}`;
+
+      axios.get(getRestaurant).then((res) => {
         // stores only first record from API
-        const askedRestr = data.restaurantDetails[0];
-        // updates state only if the Restaurant found
-        askedRestr && setRestr(askedRestr);
+        const askedRestr = res.data.restaurantDetails[0];
+
+        // updates state and searches for menu only if the Restaurant found
+        if (askedRestr) {
+          setRestr(askedRestr);
+
+          axios
+            .get(getMenu)
+            .then((res) => {
+              foundMenu(true);
+              console.log(typeof res.data);
+            })
+            // since this Menu API doesn't have data
+            .catch((err) => {
+              console.log(err);
+            });
+        }
         handleSpinner(false);
-      })
-      .catch((err) => console.log(err));
-  }, [params]);
+      });
+    },
+    //  eslint-disable-next-line
+    [params]
+  );
 
   const printData =
     selectedRestr.length !== 0 ? (
@@ -55,10 +75,11 @@ function RestaurantDetails() {
             alt={selectedRestr.restaurantName}
           />
         </div>
+        {!isMenuOfRestr && <h4>Menu is not provided by Restaurant.</h4>}
       </div>
     ) : (
       <div>
-        <p>No Such restaurant</p>
+        <p>No Such Restaurant</p>
         <button onClick={() => history.push("/")}>
           Explore other Restaurants
         </button>
--- src/variables/baseUrl.js
+++ src/variables/baseUrl.js
@@ -1,2 +1,8 @@
 export const ALL_RESTR_URL =
   "https://api.sheety.co/bdcbafbc1f4197dda178b9e69f6ccee9/techAlchemyWebTest1/allRestaurants";
+
+export const RESTR_DETAILS_URL =
+  "https://api.sheety.co/bdcbafbc1f4197dda178b9e69f6ccee9/techAlchemyWebTest1/restaurantDetails";
+
+export const RESTR_MENU_DETAILS =
+  "https://api.sheety.co/bdcbafbc1f4197dda178b9e69f6ccee9/techAlchemyWebTest1/menu";
