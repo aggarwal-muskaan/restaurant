bfaedf691bfb19ae55e30b1ecd6ba673f436bc8e
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-16 23:20:45 +0530

    styled Restaurant Details Page
    1) add icons to assets folder
    2) add styles & classnames for elements
    3) import icons and styles

--- src/components/restr-details/RestaurantDetails.js
+++ src/components/restr-details/RestaurantDetails.js
@@ -4,9 +4,19 @@ import { useParams, useHistory } from "react-router-dom";
 import { useSpinner } from "../../hooks/useSpinner";
 import { RESTR_DETAILS_URL, RESTR_MENU_DETAILS } from "../../variables/baseUrl";
 
+import { useStyle } from "../../styles/RestrDetails";
+
+import { ReactComponent as LeftIcon } from "../../assets/restrDetail/leftChevron.svg";
+import { ReactComponent as CartIcon } from "../../assets/navigationTop/filter.svg";
+import clock from "../../assets/restrDetail/clock.svg";
+import phone from "../../assets/restrDetail/phone.svg";
+import { ReactComponent as WebIcon } from "../../assets/restrDetail/web.svg";
+
 function RestaurantDetails() {
   let params = useParams();
   let history = useHistory();
+
+  const classes = useStyle();
   const [selectedRestr, setRestr] = useState([]);
   const [isMenuOfRestr, foundMenu] = useState(false);
   const [spinner, handleSpinner] = useSpinner(true);
@@ -48,39 +58,66 @@ function RestaurantDetails() {
 
   const printData =
     selectedRestr.length !== 0 ? (
-      <div>
-        <div>
-          <button onClick={() => history.goBack()}>back</button>
-          <button>cart</button>
+      <div className={classes.RestrPage}>
+        <div className={classes.HeaderBtns}>
+          <button
+            onClick={() => history.goBack()}
+            className={`${classes.goBackButton} ${classes.navigationButton}`}
+          >
+            <LeftIcon />
+          </button>
+
+          <button
+            className={`${classes.cartButton} ${classes.navigationButton}`}
+          >
+            <CartIcon />
+          </button>
         </div>
 
-        <div>
-          <div>
-            <h3>{selectedRestr.restaurantName}</h3>
+        <div className={classes.RestrDetailContainer}>
+          <div className={classes.RestrInnerContainer}>
+            <h1>{selectedRestr.restaurantName}</h1>
             <section>{selectedRestr.restaurantDescription}</section>
-            <div>
-              icon & <span>{selectedRestr.openingHours}</span>
+            <div className={classes.RestrInfo}>
+              <img src={clock} alt="clock icon" />
+              <div>
+                <p>{selectedRestr.openingHours.split(",")[0]}</p>
+                <p>{selectedRestr.openingHours.split(",")[1]}</p>
+              </div>
             </div>
-            <div>
-              icon & <span>{selectedRestr.contactNumber}</span>
+            <div className={classes.RestrInfo}>
+              <img src={phone} alt="telephone icon" />
+              <span>{selectedRestr.contactNumber}</span>
             </div>
-            <div>
-              icon & <span>{selectedRestr.websiteUrl}</span>
+            <div className={`${classes.RestrInfo} ${classes.RestrWebsiteLink}`}>
+              <WebIcon />
+              <span>{selectedRestr.websiteUrl}</span>
             </div>
           </div>
           <img
-            width="250px"
-            height="250px"
+            className={classes.RestrImage}
+            // width="250px"
+            // height="250px"
             src={selectedRestr.restaurantImage}
             alt={selectedRestr.restaurantName}
           />
         </div>
-        {!isMenuOfRestr && <h4>Menu is not provided by Restaurant.</h4>}
+        {!isMenuOfRestr && (
+          <h4 className={classes.MenuError}>
+            Menu is not provided by the Restaurant.
+          </h4>
+        )}
       </div>
     ) : (
-      <div>
-        <p>No Such Restaurant</p>
-        <button onClick={() => history.push("/")}>
+      <div className={classes.ErrorPage}>
+        <h2>
+          <em>Restaurant Not Found! </em>
+          <span>😢</span>
+        </h2>
+        <button
+          onClick={() => history.push("/")}
+          className={classes.ExploreBtn}
+        >
           Explore other Restaurants
         </button>
       </div>
---
+++ src/styles/RestrDetails.js
@@ -0,0 +1,100 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { NAVY_BLUE_COLOR, DARK_GREY, ORANGE_COLOR } from "../variables/colors";
+
+const useStyle = makeStyles({
+  RestrPage: {
+    margin: "auto",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+
+  HeaderBtns: {
+    width: "90%",
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "2.5rem 0",
+  },
+  navigationButton: {
+    borderRadius: "10px",
+    height: "48px",
+    width: "48px",
+    border: "none",
+    outline: "none",
+    cursor: "pointer",
+  },
+  goBackButton: {
+    background: NAVY_BLUE_COLOR,
+    "&:hover": {
+      filter: "brightness(120%)",
+    },
+  },
+  cartButton: {
+    background: ORANGE_COLOR,
+    "&:hover": {
+      filter: "contrast(80%)",
+    },
+  },
+  RestrDetailContainer: {
+    width: "90%",
+    display: "flex",
+    justifyContent: "space-between",
+
+    "& section, & span, & p": {
+      color: DARK_GREY,
+      margin: 0,
+    },
+  },
+
+  RestrInnerContainer: {
+    width: "47%",
+  },
+
+  RestrInfo: {
+    display: "flex",
+    alignItems: "center",
+    marginTop: "1rem",
+    "& img , & svg": {
+      marginRight: "1rem",
+    },
+  },
+
+  RestrWebsiteLink: {
+    cursor: "pointer",
+    "&:hover": {
+      "& span": {
+        color: ORANGE_COLOR,
+      },
+    },
+  },
+
+  RestrImage: {
+    width: "50%",
+    borderRadius: "16px",
+    backgroundSize: "cover",
+    objectFit: "cover",
+  },
+
+  MenuError: {
+    fontStyle: "italic",
+    color: ORANGE_COLOR,
+  },
+
+  ErrorPage: {
+    textAlign: "center",
+  },
+
+  ExploreBtn: {
+    borderRadius: "6px",
+    padding: "1rem",
+    border: "none",
+    outline: "none",
+    backgroundColor: ORANGE_COLOR,
+    cursor: "pointer",
+    color: "#fff",
+    fontSize: "20px",
+    fontWeight: 600,
+  },
+});
+
+export { useStyle };
