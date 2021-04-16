128f89c7a65f9a8975911039f28fa5dc2d437529
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-16 20:40:36 +0530

    :tada: styles responsive Cards

--- src/components/restaurant/RestaurantCards.js
+++ src/components/restaurant/RestaurantCards.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { useStyle } from "../../styles/RestaurantCards";
 import { useHistory } from "react-router-dom";
+
+import TextClamp from "react-string-clamp";
 
 function RestaurantCards({
   id,
@@ -10,20 +13,34 @@ function RestaurantCards({
 }) {
   //routing property to redirect within the application
   const history = useHistory();
+  const classes = useStyle();
 
   return (
-    <div onClick={() => history.push(`/${id}`)}>
+    <div onClick={() => history.push(`/${id}`)} className={classes.CardInfo}>
       <img
         src={restaurantImage}
         alt={`${restaurantName}`}
         width="230px"
         height="230px"
+        className={classes.CardImage}
       />
-      <div>
+      <div className={classes.CardHeader}>
         <h4>{restaurantName}</h4>
-        <span>{isOpen ? "Open Now" : "Closed"}</span>
+
+        <span
+          className={
+            `${classes.RestrLabel} ` + (!isOpen && `${classes.RestrClosed}`)
+          }
+        >
+          {isOpen ? "Open Now" : "Closed"}
+        </span>
       </div>
-      <p>{restaurantDescription}</p>
+      <TextClamp
+        element="p"
+        lines="3"
+        className={classes.CardContent}
+        text={restaurantDescription}
+      />
     </div>
   );
 }
---
+++ src/styles/RestaurantCards.js
@@ -0,0 +1,48 @@
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  LIGHT_PINK,
+  ORANGE_COLOR,
+  LIGHT_BLUE,
+  NAVY_BLUE_COLOR,
+} from "../variables/colors";
+
+const useStyle = makeStyles({
+  CardInfo: {
+    minWidth: "300px",
+    maxWidth: "330px",
+  },
+  CardImage: {
+    width: "100%",
+    minHeight: "150px",
+
+    maxHeight: "170px",
+    backgroundSize: "cover",
+    borderRadius: "16px",
+  },
+  CardHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    margin: "15px 0",
+    "& h4": {
+      margin: 0,
+    },
+  },
+  RestrLabel: {
+    fontWeight: 600,
+    padding: "4px 8px",
+    borderRadius: "4px",
+    fontSize: "11px",
+    background: LIGHT_BLUE,
+    color: NAVY_BLUE_COLOR,
+  },
+
+  RestrClosed: {
+    background: LIGHT_PINK,
+    color: ORANGE_COLOR,
+  },
+
+  CardContent: {
+    fontSize: "12px",
+  },
+});
+export { useStyle };
