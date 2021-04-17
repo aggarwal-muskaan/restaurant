e697c6f0e0f7d8d0e2613cc39970e0ba617126bb
Muskaan Aggarwal <muskaan19010@gmail.com>
2021-04-17 18:19:24 +0530

    :heavy_plus_sign: add new file (.gif)

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import { ReactComponent as MenuIcon } from "../assets/navigationTop/toggle.svg";
+import { toggleMobileSidebar } from "../contexts/sidebar.context";
+import { makeStyles } from "@material-ui/core/styles";
+import breakpoints from "../variables/breakpoint";
+
+const useStyle = makeStyles({
+  menuButton: {
+    [breakpoints.up("sm")]: {
+      display: "none !important",
+    },
+  },
+});
 
 function PageNotFound() {
+  const handleDrawerToggle = useContext(toggleMobileSidebar);
+  const classes = useStyle();
   return (
-    <div>
-      <h2>Coming Soon...</h2>
+    <div style={{ width: "100%" }}>
+      <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        edge="start"
+        onClick={() => handleDrawerToggle(true)}
+        className={classes.menuButton}
+      >
+        <MenuIcon />
+      </IconButton>
+      <h2 style={{ textAlign: "center", color: "green" }}>Coming Soon...</h2>
     </div>
   );
 }
